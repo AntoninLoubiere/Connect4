ac54203747390c5c4ed87209ee9bd45952b0f6c5
Antonin Loubiere <antonin.loubiere@laposte.net>
2019-07-07 11:58:00 +0200

    Start to implement game class
    Start to implement game class, create a function which  add the tile of the current player in a column

--- Game.py
+++ Game.py
@@ -17,12 +17,35 @@ class Game(object):
         """
 
         self.grid = []
-        for y in range(0, GRID_HEIGHT):
+        for x in range(GRID_WIDTH):
             self.grid.append([])
-            for x in range(0, GRID_WIDTH):
-                self.grid[y].append(TileState.Blank)
+            for y in range(GRID_HEIGHT):
+                self.grid[x].append(TileState.Blank)
 
         self.current_turn = (TileState.Player_1, TileState.Player_2)[random.randint(0, 1) == 0]
+
+    def add_tile(self, column):
+        """
+        Add the tile of the current player in a column
+        :param column: the column which you would add the tile
+        :return: if this action was do
+        """
+
+        for y in range(len(self.grid[column]) - 1, 0 - 1, -1):
+            if self.grid[column][y] == TileState.Blank:
+                #  add tile
+                self.grid[column][y] = self.current_turn
+                self.swap_turn()
+                return True
+
+        return False
+
+    def swap_turn(self):
+        """
+        Swap the turn (Player 1 to 2 and 2 to 1)
+        :return: None
+        """
+        self.current_turn = (TileState.Player_1, TileState.Player_2)[self.current_turn == TileState.Player_1]
 
     def __str__(self):
         """
@@ -31,10 +54,10 @@ class Game(object):
         """
         string = "Grid: \n"
 
-        for iy, y in enumerate(self.grid):
-            for ix, x in enumerate(y):
-                string += "{:^8}".format(x.name)
-                string += (" | ", "\n")[ix == len(y) - 1]
+        for y in range(GRID_HEIGHT):
+            for x in range(GRID_WIDTH):
+                string += "{:^8}".format(self.grid[x][y].name)
+                string += (" | ", "\n")[x == GRID_WIDTH - 1] # if is the last element
 
         string += "\nLe tour est à: " + self.current_turn.name
 
--- Power4.py
+++ Power4.py
@@ -18,6 +18,7 @@ def main():
     """
 
     game = Game()
+
     print(str(game))
 
 
